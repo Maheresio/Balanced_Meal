c6494080d142769aaa17b3d78dbdf0ccbb591a38
Maheresio <zamalek.545r5@gmail.com>
2025-02-25 09:41:25 +0200

    remove unused imports in boarding view

--- lib/features/boarding/widgets/boarding_view_body.dart
+++ lib/features/boarding/widgets/boarding_view_body.dart
@@ -1,11 +1,7 @@
-import 'package:balanced_meal/core/utils/app_colors.dart';
 import 'package:balanced_meal/core/utils/app_images.dart';
-import 'package:balanced_meal/core/utils/app_strings.dart';
-import 'package:balanced_meal/core/utils/app_styles.dart';
 import 'package:balanced_meal/features/boarding/widgets/boarding_footer_section.dart';
 import 'package:balanced_meal/features/boarding/widgets/boarding_header_section.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class BoardingViewBody extends StatelessWidget {
   const BoardingViewBody({super.key});
