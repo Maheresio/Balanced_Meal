a4ceb9780ecfd91ae64e7d4fd8175307e08e38ce
Maheresio <zamalek.545r5@gmail.com>
2025-02-25 08:02:20 +0200

    adding boarding view

--- lib/core/utils/app_router.dart
+++ lib/core/utils/app_router.dart
@@ -1,20 +1,16 @@
+import 'package:balanced_meal/features/boarding/boarding_view.dart';
 import 'package:go_router/go_router.dart';
 
 abstract class AppRouter {
   static const kHomeView = '/homeView';
+  static const kBoardingView = '/boardingView';
 
   static final GoRouter router = GoRouter(
     routes: [
       GoRoute(
         path: '/',
         builder: (context, state) {
-          return const SplashView();
-        },
-      ),
-      GoRoute(
-        path: kHomeView,
-        builder: (context, state) {
-          return const HomeView();
+          return const BoardingView();
         },
       ),
     ],
---
+++ lib/features/boarding/boarding_view.dart
@@ -0,0 +1,11 @@
+import 'package:balanced_meal/features/boarding/widgets/boarding_view_body.dart';
+import 'package:flutter/material.dart';
+
+class BoardingView extends StatelessWidget {
+  const BoardingView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: BoardingViewBody());
+  }
+}
