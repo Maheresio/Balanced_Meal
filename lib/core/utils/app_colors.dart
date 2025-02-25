1d71d0dab3a506391ab4a1d4c90420ec371d89aa
Maheresio <zamalek.545r5@gmail.com>
2025-02-25 09:23:24 +0200

    add kLightWhiteColor to AppColors for improved color palette

--- lib/core/utils/app_colors.dart
+++ lib/core/utils/app_colors.dart
@@ -4,6 +4,7 @@ abstract class AppColors {
   static final kPrimaryColor = Color(0xFFF25700);
   static final kBlackColor = Color(0xFF1E1E1E);
   static final kWhiteColor = Color(0xFFFFFFFF);
+  static final kLightWhiteColor = Color(0xFFDADADA);
   static final kLightGreyColor = Color(0xFF959595);
   static final kDarkGreyColor = Color(0xFF474747);
   static final kSilverColor = Color(0xFFEAECF0);
