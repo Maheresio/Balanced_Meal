f973a561a9c0a8c8ad69533d1347b20464ea96ee
Maheresio <zamalek.545r5@gmail.com>
2025-02-25 08:49:11 +0200

    refactor app styles

--- lib/core/utils/app_styles.dart
+++ lib/core/utils/app_styles.dart
@@ -5,34 +5,34 @@ import 'app_colors.dart';
 
 abstract class AppStyles {
   //questrual font pay attention
-  static TextStyle textStyleRegular16(BuildContext context) => TextStyle(
+  static TextStyle get textStyleRegular16 => TextStyle(
     fontSize: 16.sp,
     color: AppColors.kDarkGreyColor,
     fontWeight: FontWeight.w400,
   );
-  static TextStyle textStyleMedium16(BuildContext context) => TextStyle(
+  static TextStyle get textStyleMedium16 => TextStyle(
     fontSize: 16.sp,
     color: AppColors.kDarkGreyColor,
     fontWeight: FontWeight.w500,
   );
 
-  static TextStyle textStyleRegular14(BuildContext context) => TextStyle(
+  static TextStyle get textStyleRegular14 => TextStyle(
     fontSize: 14.sp,
     color: AppColors.kLightGreyColor,
     fontWeight: FontWeight.w400,
   );
-  static TextStyle textStyleMedium14(BuildContext context) => TextStyle(
+  static TextStyle get textStyleMedium14 => TextStyle(
     fontSize: 14.sp,
     color: AppColors.kDarkGreyColor,
     fontWeight: FontWeight.w500,
   );
-  static TextStyle textStyleMedium20(BuildContext context) => TextStyle(
+  static TextStyle get textStyleMedium20 => TextStyle(
     fontSize: 20.sp,
     color: AppColors.kBlackColor,
     fontWeight: FontWeight.w500,
   );
 
-  static TextStyle textStyleExtraBold48(BuildContext context) => TextStyle(
+  static TextStyle get textStyleExtraBold48 => TextStyle(
     fontSize: 48.sp,
     color: AppColors.kWhiteColor,
     fontWeight: FontWeight.w700,
