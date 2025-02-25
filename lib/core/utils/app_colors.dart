091e114dad341d5e687d7fdd5b20426151e7b8aa
Maheresio <zamalek.545r5@gmail.com>
2025-02-25 07:31:19 +0200

    refactor appColors

--- lib/core/utils/app_colors.dart
+++ lib/core/utils/app_colors.dart
@@ -1,13 +1,11 @@
 import 'package:flutter/material.dart';
 
-class AppColors {
-  AppColors._();
-
-  final kPrimaryColor = Color(0xFFF25700);
-  final kBlackColor = Color(0xFF1E1E1E);
-  final kWhiteColor = Color(0xFFFFFFFF);
-  final kLightGreyColor = Color(0xFF959595);
-  final kDarkGreyColor = Color(0xFF474747);
-  final kSilverColor = Color(0xFFEAECF0);
-  final kPinkColor = Color(0xFFFFEDE3);
+abstract class AppColors {
+  static final kPrimaryColor = Color(0xFFF25700);
+  static final kBlackColor = Color(0xFF1E1E1E);
+  static final kWhiteColor = Color(0xFFFFFFFF);
+  static final kLightGreyColor = Color(0xFF959595);
+  static final kDarkGreyColor = Color(0xFF474747);
+  static final kSilverColor = Color(0xFFEAECF0);
+  static final kPinkColor = Color(0xFFFFEDE3);
 }
