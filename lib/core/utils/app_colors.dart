37c5ae00341435020fe4866ef0e66d1b300073bc
Maheresio <zamalek.545r5@gmail.com>
2025-02-25 07:15:13 +0200

    add app colors

---
+++ lib/core/utils/app_colors.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  AppColors._();
+
+  final kPrimaryColor = Color(0xFFF25700);
+  final kBlackColor = Color(0xFF1E1E1E);
+  final kWhiteColor = Color(0xFFFFFFFF);
+  final kLightGreyColor = Color(0xFF959595);
+  final kDarkGreyColor = Color(0xFF474747);
+  final kSilverColor = Color(0xFFEAECF0);
+  final kPinkColor = Color(0xFFFFEDE3);
+}
