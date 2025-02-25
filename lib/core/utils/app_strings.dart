6699c3dc13e661f46a5e2f4a851932213ecd3b2f
Maheresio <zamalek.545r5@gmail.com>
2025-02-25 09:23:12 +0200

    add AppStrings utility class for consistent string management

---
+++ lib/core/utils/app_strings.dart
@@ -0,0 +1,6 @@
+abstract class AppStrings {
+  static const String kBalancedMeal = 'Balanced Meal';
+  static const String kOrderFood = 'Order Food';
+  static const String kCraftYourMeal =
+      'Craft your ideal meal effortlessly with our app. Select nutritious ingredients tailored to your taste and well-being.';
+}
