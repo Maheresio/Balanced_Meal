efc9ef70d08d02ab1233cffb70ad04090cda3ec7
Maheresio <zamalek.545r5@gmail.com>
2025-03-04 13:12:34 +0200

    add DetailsView and DetailsViewBody

---
+++ lib/features/details/details_view.dart
@@ -0,0 +1,11 @@
+import 'package:balanced_meal/features/details/widgets/details_view_body.dart';
+import 'package:flutter/material.dart';
+
+class DetailsView extends StatelessWidget {
+  const DetailsView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: DetailsViewBody());
+  }
+}
