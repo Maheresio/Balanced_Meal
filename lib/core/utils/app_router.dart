9906ad3e9a4712768d752c29a9671847868c6e53
Maheresio <zamalek.545r5@gmail.com>
2025-03-04 13:12:20 +0200

    add details view route to AppRouter

--- lib/core/utils/app_router.dart
+++ lib/core/utils/app_router.dart
@@ -1,9 +1,11 @@
 import 'package:balanced_meal/features/boarding/presentation/boarding_view.dart';
+import 'package:balanced_meal/features/details/details_view.dart';
 import 'package:go_router/go_router.dart';
 
 abstract class AppRouter {
   static const kHomeView = '/homeView';
   static const kBoardingView = '/boardingView';
+  static const kDetailsView = '/detailsView';
 
   static final GoRouter router = GoRouter(
     routes: [
@@ -11,6 +13,12 @@ abstract class AppRouter {
         path: '/',
         builder: (context, state) {
           return BoardingView();
+        },
+      ),
+      GoRoute(
+        path: kDetailsView,
+        builder: (context, state) {
+          return DetailsView();
         },
       ),
     ],
